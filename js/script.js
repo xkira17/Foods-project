1cb895bd856eaf7ba780b41874e27b81a4b6636c
securtto <kbkabulov0225@gmail.com>
2025-01-19 22:10:54 +0500

    api doesn't work without vpn

--- js/script.js
+++ js/script.js
@@ -1,32 +1,44 @@
-const searchInput = document.querySelector('#search')
-const main = document.querySelector('#main')
+const searchInput = document.querySelector("#search");
+const main = document.querySelector("#main");
 
-// let searchText = "Chicken"
+let searchText = "Chicken";
 
 function fetchAllFoods(search) {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
-    .then(res => res.json())
-    .then(data => renderFoods(data.meals))
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      return res.json();
+    })
+    .then((data) => renderFoods(data.meals))
+
+    .catch((err) => {
+      main.innerHTML = `<h1 class="error" style="padding: 20px ; color: red;">Failed to fetch: ${err.message}</h1>`;
+      console.error(err);
+    });
 }
 
-fetchAllFoods('Chicken')
+fetchAllFoods("a");
 
 function renderFoods(array) {
-    main.innerHTML = ''
-    if(!array) {
-        main.innerHTML = `<h1 class="error">😕Oops...</h1>`
-    }
-    array.map(item => {
-        main.innerHTML += `
-            <div class="foods-content">
-                <img src="${item.strMealThumb}">
-                <h2>${item.strMeal}</h2>
+  main.innerHTML = "";
+  if (!array) {
+    main.innerHTML = `<h1 class="error">😕Oops...</h1>`;
+  }
+  array.map((item) => {
+    main.innerHTML += `
+            <div class="card">
+                    <img src="${item.strMealThumb}">
+                <div class="card-content" >
+                    <h2>${item.strMeal}</h2>
+                </div>
             </div>
-        `
-    })
+        `;
+  });
 }
 
-searchInput.addEventListener('change', e => {
-    fetchAllFoods(searchInput.value)
-    searchText = searchInput.value
-})
+searchInput.addEventListener("change", () => {
+  fetchAllFoods(searchInput.value);
+  searchText = searchInput.value;
+});
